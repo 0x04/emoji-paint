09be796f7dbe27e86c80e6d597ee5b257e20756d
0x04 <ok@0x04.de>
2026-05-05 23:37:07 +0200

    chore: bump version

--- src/constants/versionString.mjs
+++ src/constants/versionString.mjs
@@ -1,3 +1,5 @@
-export const versionString = '0.9.0'
+import { version as versionString } from '../../package.json'
+
 const { 0: major, 1: minor, 2: patch } = versionString.split('.')
 export const versionObject = { major, minor, patch }
+export { versionString }
